5273b23495a4c256e26636f72adf3fd5097a0e2f
miguel garzon <mig.garzon@udla.edu.co>
2025-12-02 16:39:11 -0500

    credenciales

--- course_sync_service/app/shared/credentials.py
+++ course_sync_service/app/shared/credentials.py
@@ -17,24 +17,19 @@ class SharedContainer(containers.DeclarativeContainer):
         "https://www.googleapis.com/auth/classroom.rosters",
     ]
 
-    # Detecta si estamos en LOCAL (archivo JSON) o CLOUD RUN (credenciales por defecto)
     def _load_credentials():
         json_path = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
 
         if json_path:  
-            # ⭐ MODO LOCAL
             return service_account.Credentials.from_service_account_file(
                 json_path,
                 scopes=SharedContainer.GOOGLE_SCOPES,
             )
         
-        # ⭐ MODO CLOUD RUN
         creds, _ = google_auth_default(scopes=SharedContainer.GOOGLE_SCOPES)
         return creds
 
     google_credentials = providers.Singleton(_load_credentials)
-
-    # Impersonation
     impersonated_credentials = providers.Singleton(
         lambda creds: creds.with_subject(os.getenv("GOOGLE_IMPERSONATE_ADMIN")),
         creds=google_credentials
